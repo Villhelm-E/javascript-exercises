1cc08fbfa0e33f8d84db6f94e0a9ec81854e8d18
villhelm-e <villhelm.e@gmail.com>
2021-02-08 08:58:47 -0800

    Completed repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(message, repeats) {
+    let modString = "";
+    
+    if (repeats < 0) {
+        modString = "ERROR";
+    } else {
+        for (let i = 1; i <= repeats; i++) {
+            modString += message;
+        }
+    }
 
+    return modString;
 }
 
 module.exports = repeatString
