4a95956efa0a2e1d764c9620b3089679217d1232
villhelm-e <villhelm.e@gmail.com>
2021-02-08 09:23:11 -0800

    completed reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //split string into a new array
+    let newStr = ""
+    let chArr = str.split("");
+    //loop the array in reverse
+    for (let i = chArr.length-1; i >= 0; i--) {
+        newStr += chArr[i];
+    }
+    
+    return newStr;
 }
 
 module.exports = reverseString
