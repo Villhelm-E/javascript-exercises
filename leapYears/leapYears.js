0c9a1fb1699c24ac31364dba1ae2a1e9bc6e9d6a
villhelm-e <villhelm.e@gmail.com>
2021-02-09 09:38:20 -0800

    completed leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if year is divisible by 4 and 100 and 400 or year is divisible by 4 and not 100 or 400, it's a leap year
+    //tested combinations in an Excel sheet
+    if ((year % 4 == 0 && year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0 && year % 400 != 0)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
