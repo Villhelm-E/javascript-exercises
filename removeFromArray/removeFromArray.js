6ac0cd333834a17adf6738cd27e121eeec279e72
villhelm-e <villhelm.e@gmail.com>
2021-02-09 08:24:03 -0800

    completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
 
+    //loop through args
+    for (i = 1; i < arguments.length; i++) {
+        for (ii = 0; ii < array.length; ii++) {
+            if (array[ii] === arguments[i]) {
+                array.splice(ii, 1);
+            }
+        }
+    }
+
+    return array;
 }
 
 module.exports = removeFromArray
