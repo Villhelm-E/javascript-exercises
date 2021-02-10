b9b45771d5a244dcc73acf58042c7705de429d7d
villhelm-e <villhelm.e@gmail.com>
2021-02-10 08:18:20 -0800

    completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(farenheit) {
+  const conv = (farenheit - 32) * (5/9);
 
+  return Math.round(conv * 10) / 10;
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  const conv = (celsius * (9/5)) + 32;
 
+  return Math.round(conv * 10) / 10;
 }
 
 module.exports = {
