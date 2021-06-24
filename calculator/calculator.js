8ec9294bc7f492ddaec5c06b736a7d1066e53cd7
villhelm-e <villhelm.e@gmail.com>
2021-06-24 14:23:12 -0700

    Completed calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,52 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (arr) {
+	result = 0;
+	for (let i in arr) {
+		result += arr[i];
+	}
+
+	return result;
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	result = 1;
+
+	for (let i in arr) {
+		result *= arr[i];
+	}
+
+	return result;
 }
 
-function power() {
-	
+function power(base, power) {
+	result = base;
+	for (i = 0; i < (power - 1); i++) {
+		result *= base;
+	}
+
+	return result;
 }
 
-function factorial() {
+function factorial(num) {
 	
+	//0 is a special case
+	if (num == 0) {	
+		return 1;
+	}
+	//every other positive integer
+	else {
+		for (i = (num - 1); i > 1; i--) {
+			num *= i;
+		}
+	}
+	return num;
 }
 
 module.exports = {
