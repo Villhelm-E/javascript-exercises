23854a3caa376fece16cff01bba5e4c7d0e45e13
villhelm-e <villhelm.e@gmail.com>
2021-06-25 13:54:53 -0700

    Completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(instance) {
+    //set up variables
+    a = 1; //could be anything, it gets replaced. not my best code
+    b = 1;
+    c = 0;
 
+    //check for negative input
+    if (instance < 0 || isNaN(instance)) {
+        return "OOPS";
+    }
+    else {
+        for (let i = 0; i < instance; i++) {
+            
+            //shift variables
+            a = b;
+            b = c;
+            
+            //add to get next number
+            c = a + b;
+        }
+        
+        return c;
+    }
 }
 
 module.exports = fibonacci
