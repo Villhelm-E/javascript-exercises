ad7e7ab99bc435db7ffdae6db4d7b54c9a824676
villhelm-e <villhelm.e@gmail.com>
2021-06-28 08:44:43 -0700

    Completed getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    //create an empty array
+    var titles = [];
+    
+    //iterate through the array and add its book title to titles array
+    for (i = 0; i < arr.length; i++) {
+        titles.push(arr[i].title);
+    }
 
+    //return the titles array
+    return titles;
 }
 
 module.exports = getTheTitles;
