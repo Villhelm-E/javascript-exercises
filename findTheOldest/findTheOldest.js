2ce3749a5267e4ce141c2bbcce18c5922d581ecf
villhelm-e <villhelm.e@gmail.com>
2021-06-29 08:44:57 -0700

    Making use of array.map() instead, and generally shrinking code

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -2,32 +2,20 @@ let findTheOldest = function(people) {
     //Variables
     var oldest = 0; //as index of people[]
     d = new Date();
-    let ages = [];
 
     //loops through people[] and adds each item's age to ages[]
-    for (i = 0; i < people.length; i++) {
-        
-        //check for death year
-        if (people[i].yearOfDeath == undefined) {
-            //calculate age from current year
-            age = d.getFullYear(Date.now()) - people[i].yearOfBirth;
+    const ages = people.map(person => {
+        if (!person.yearOfDeath) {
+            return d.getFullYear(Date.now()) - person.yearOfBirth;
         }
         else {
-            //calculate age from death year
-            age = people[i].yearOfDeath - people[i].yearOfBirth
-        }
-        
-        //add calculated age to ages[]
-        ages.push(age);
-
-    }
+            return person.yearOfDeath - person.yearOfBirth;
+        }})
 
     //loop through ages to find the oldest
     for (i = 0; i < people.length; i++) {
         //only update oldest when loop finds a larger age
-        if (ages[i] > ages[oldest]) {
-            oldest = i;
-        }
+        (ages[i] > ages[oldest]) && (oldest = i);
     }
 
     //return the correct element in people[] using 'oldest' as an index
